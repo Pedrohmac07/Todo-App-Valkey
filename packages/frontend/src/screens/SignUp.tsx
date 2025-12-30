96a04a132c3a1ad009165f145e89a1b9628f4334
Pedro Machado <ph374011@gmail.com>
2025-12-30 12:57:07 -0300

    added SignUp screen

---
+++ packages/frontend/src/screens/SignUp.tsx
@@ -0,0 +1,23 @@
+  ~/Estudos/todo-cache/packages/frontend  ▲            Size        Type         Last Modified  
+  󰉋 public                                                 -   directory     2025-12-30 12:44   
+   src                                                    -   directory     2025-12-30 12:44   
+  │ 󰉋 assets                                               -   directory     2025-12-30 12:44   
+  │ 󰉋 components                                           -   directory     2025-12-30 12:44   
+  │ 󰉋 context                                              -   directory     2025-12-30 12:44   
+  │ 󰉋 interfaces                                           -   directory     2025-12-30 12:44   
+  │ 󰉋 screens                                              -   directory     2025-12-30 12:44   
+  │ 󰉋 services                                             -   directory     2025-12-30 12:44   
+  │ 󰉋 utils                                                -   directory     2025-12-30 12:44   
+  │ 󰌜 App.css                                          627 B         css     2025-12-30 12:44   
+  │  App.tsx                                        1.01 KB         tsx     2025-12-30 12:44   
+  │ 󰌜 index.css                                      1.13 KB         css     2025-12-30 12:44   
+  └  main.tsx                                         230 B         tsx     2025-12-30 12:44   
+   README.md                                        2.46 KB          md     2025-12-30 12:44   
+  󰌞 eslint.config.js                                   616 B          js     2025-12-30 12:44   
+  󰌝 index.html                                         408 B        html     2025-12-30 12:44   
+  󰘦 package.json                             ✗        932 B        json     2025-12-30 12:43   
+  󰘦 tsconfig.app.json                                  732 B        json     2025-12-30 12:44   
+  󰘦 tsconfig.json                                      119 B        json     2025-12-30 12:44   
+  󰘦 tsconfig.node.json                                 653 B        json     2025-12-30 12:44   
+  󰛦 vite.config.ts                                     237 B          ts     2025-12-30 12:44   
+  (3 hidden items)
